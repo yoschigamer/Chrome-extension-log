41c5e99e8465a785b1851b0efa5a935a77289da0
Killian Schneider <80042061+yoschigamer@users.noreply.github.com>
2024-10-21 12:52:40 +0200

    Add files via upload

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
+
 chrome.storage.local.set({ DOLAPIKEY: "undefined" }).then(() => {
   console.log("Waiting For Key");
 });
+
+// cest pour ca que jutilise le stockage de google  et faut forcement que le gars mette sa propre clé fin au mieux
